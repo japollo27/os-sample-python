fd5dc2852607d0c3c4283a2620364f7e533f91c6
japollo27 <jonathanabolins@gmail.com>
2018-07-21 11:47:40 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+@application.route("/dashboard")
+def hello():
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
     application.run()
